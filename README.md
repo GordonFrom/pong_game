792d6706294d2634e3c1a73d8af7c02a8c5c884c
const <ksimanov@gmail.com>
2014-02-16 16:22:54 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Ruby on Rails Tutorial
+
+Это пример приложения для
+[*http://railstutorial.ru/*](http://http://railstutorial.ru/)
---
+++ game.rb
@@ -0,0 +1,109 @@
+require 'rubygems'
+require 'rubygame'
+
+class Game
+    def initialize
+        @screen = Rubygame::Screen.new [640, 480], 0, [Rubygame::HWSURFACE, Rubygame::DOUBLEBUF]
+        @screen.title = "Pong"
+        
+        @queue = Rubygame::EventQueue.new
+        @clock = Rubygame::Clock.new
+        @clock.target_framerate = 60
+        
+        @player = Paddle.new 50, 10
+        @enemy = Paddle.new @screen.width - 50 - @player.width, 10
+        @player.center_y @screen.height
+        @enemy.center_y @screen.height
+        @background = Background.new @screen.width, @screen.height
+    end
+    
+    def run!
+        loop do
+            update
+            draw
+            @clock.tick
+        end
+    end
+    
+    def update
+        @queue.each do |ev|
+            case ev
+                when Rubygame::QuitEvent
+                    Rubygame.quit
+                    exit
+            end
+        end
+    end
+    
+    def draw
+        @screen.fill [0,0,0]
+        
+        @background.draw @screen
+        @player.draw @screen
+        @enemy.draw @screen
+
+        @screen.flip
+    end
+end
+
+class GameObject
+    attr_accessor :x, :y, :width, :height, :surface
+    
+    def initialize x, y, surface
+        @x = x
+        @y = y
+        @surface = surface
+        @width = surface.width
+        @height = surface.height
+    end
+    
+    def update
+    end
+    
+    def draw screen
+        @surface.blit screen, [@x, @y]
+    end
+    
+    def handle_event event
+    end
+end
+
+class Paddle < GameObject
+    def initialize x, y
+        surface = Rubygame::Surface.new [20, 100]
+        surface.fill [255, 255, 255]
+        super x, y, surface
+    end
+    
+    def center_y h
+        @y = h/2-@height/2
+    end
+end
+
+class Background < GameObject
+    def initialize width, height
+        surface = Rubygame::Surface.new [width, height]
+        
+        # Draw background
+        white = [255, 255, 255]
+        
+        # Top
+        surface.draw_box_s [0, 0], [surface.width, 10], white
+        # Left
+        surface.draw_box_s [0, 0], [10, surface.height], white
+        # Bottom
+        surface.draw_box_s [0, surface.height-10], [surface.width,
+surface.height], white
+        # Right
+        surface.draw_box_s [surface.width-10, 0], [surface.width,
+surface.height], white
+        # Middle Divide
+        surface.draw_box_s [surface.width/2-5, 0],
+[surface.width/2+5, surface.height], white
+        
+        super 0, 0, surface
+    end 
+end
+
+g = Game.new
+g.run!
